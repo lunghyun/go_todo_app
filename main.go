564794d287126af24e13cf9e8ab9e8de9e0d76a8
lunghyun <nangmannsh@gmail.com>
2026-02-19 16:41:46 +0900

    feat: errgroup으로 종료 알림 기다리는 run 함수 구현

--- main.go
+++ main.go
@@ -2,29 +2,46 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
-	// HTTP 서버 실행 시, *http.Server 타입을 경유하는 것이 정석
-	// - Shutdown 메서드 존재
-	// - ListenAndServe 존재
-	// - ctx로 외부에서 취소 처리를 받을 수 있음
 	s := &http.Server{
 		Addr: ":8080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			_, _ = fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
 	}
-	_ = s.ListenAndServe()
 
-	return nil
+	eg, ctx := errgroup.WithContext(ctx)
+	// 다른 고루틴에서 HTTP 서버를 실행
+	eg.Go(func() error {
+		// http.ErrServerClosed:
+		// Shutdown이 정상 종료된 것을 나타내므로 이상 처리가 아님
+		if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// ch로부터 종료 알림을 기다림
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// Go 메서드로 실행한 다른 goroutine의 종료를 기다림
+	return eg.Wait()
 }
 
 func main() {
-	if err := run(context.TODO()); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 	}
 }
