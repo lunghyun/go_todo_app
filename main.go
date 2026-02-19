0a22d763d5b957af27d5bbc59983cacaba976f18
lunghyun <nangmannsh@gmail.com>
2026-02-19 16:15:42 +0900

    feat: main에서 run으로 처리 분리 패턴 구현 - *http.Server.ListenAndServe를 통해 HTTP 요청을 받음 - 인수로 전달한 ctx를 통해 Shutdown 메서드를 통해 처리 중단 명령을 HTTP 서버 기능 종료로 이용 - run 반환값으로 오류 처리

--- main.go
+++ main.go
@@ -1,27 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 )
 
-// 요청을 받아 응답 메시지를 생성하는 서버
-// 포트번호 8080 고정
-
-func main() {
-
-	// ListenAndServe:
-	// arg1: 주소 문자열(IP 정보 생략 -> local)
-	// arg2: 핸들러(단일 처리만 구현 -> 어떤 path든 path를 통한 응답 메시지 반환
-	err := http.ListenAndServe(":8080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(ctx context.Context) error {
+	// HTTP 서버 실행 시, *http.Server 타입을 경유하는 것이 정석
+	// - Shutdown 메서드 존재
+	// - ListenAndServe 존재
+	// - ctx로 외부에서 취소 처리를 받을 수 있음
+	s := &http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			_, _ = fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
-	)
-	// 기본적으로 병렬 요청 처리됨
-	if err != nil {
-		fmt.Printf("failed to start server: %v", err)
-		os.Exit(1)
+	}
+	_ = s.ListenAndServe()
+
+	return nil
+}
+
+func main() {
+	if err := run(context.TODO()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
 }
