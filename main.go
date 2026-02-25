b8b40f3eb06a7b400f5480361fba8f468b72344e
lunghyun <nangmannsh@gmail.com>
2026-02-26 02:28:39 +0900

    feat(signal): 종료 시그널 감지에 따라 정상 종료 구현 및 err 변수 셰도잉 문제 수정

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/lunghyun/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
@@ -21,6 +24,8 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -34,6 +39,8 @@ func run(ctx context.Context) error {
 	s := &http.Server{
 		// 인수로 받은 net.Listener를 이용하므로 Addr 필드는 지정하지 않는다.
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// 명령줄에서 테스트하기 위한 로직
+			time.Sleep(5 * time.Second)
 			_, _ = fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
 	}
@@ -42,7 +49,7 @@ func run(ctx context.Context) error {
 	eg.Go(func() error {
 		// ListenAndServe -> Serve로 변경
 		// http.ErrServerClosed는 httpServerShutdown이 정상 종료되었다고 표시하므로 문제없음
-		if err := s.Serve(l); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		if err = s.Serve(l); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			log.Printf("failed to close: %+v", err)
 			return err
 		}
@@ -51,7 +58,7 @@ func run(ctx context.Context) error {
 
 	// ch로부터 종료 알림을 기다림
 	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
+	if err = s.Shutdown(context.Background()); err != nil {
 		log.Printf("failed to shutdown: %+v", err)
 	}
 
