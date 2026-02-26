b2c707e93057bc72ecbf3e79471b4b2eb3eef3f3
lunghyun <nangmannsh@gmail.com>
2026-02-26 16:38:38 +0900

    feat(endpoint): entity.Task 타입값을 영구 저장하는 기능의 로직 확인을 위한 임시 구현

---
+++ store/store.go
@@ -0,0 +1,40 @@
+package store
+
+import (
+	"errors"
+
+	"github.com/lunghyun/go_todo_app/entity"
+)
+
+var (
+	Tasks       = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
+	ErrNotFound = errors.New("not found")
+)
+
+type TaskStore struct {
+	// 동작 확인용 -> export하는중
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
+
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return t.ID, nil
+}
+
+func (ts *TaskStore) Get(id entity.TaskID) (*entity.Task, error) {
+	if t, ok := ts.Tasks[id]; ok {
+		return t, nil
+	}
+	return nil, ErrNotFound
+}
+
+func (ts *TaskStore) All(id entity.TaskID) entity.Tasks {
+	tasks := make([]*entity.Task, len(ts.Tasks))
+	for i, t := range ts.Tasks {
+		tasks[i-1] = t
+	}
+	return tasks
+}
