f5ea6f08e7569fd795b49c3f80b02f755abb36da
lunghyun <nangmannsh@gmail.com>
2026-02-27 19:01:20 +0900

    feat(endpoint): 충돌 가능성이 있는 mux.Handle 제거

--- mux.go
+++ mux.go
@@ -16,13 +16,12 @@ func NewMux() http.Handler {
 		w.Header().Set("Content-type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status":"ok"}`))
 	})
-	// +
+
 	v := validator.New()
-	mux.Handle("/tasks", &handler.AddTask{Store: store.Tasks, Validator: v})
 	at := &handler.AddTask{Store: store.Tasks, Validator: v}
 	mux.Post("/tasks", at.ServeHTTP)
 	lt := &handler.ListTask{Store: store.Tasks}
 	mux.Get("/tasks", lt.ServeHTTP)
-	// +
+
 	return mux
 }
