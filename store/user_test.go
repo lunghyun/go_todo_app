72657fb7c0502f1a912adc64e74dd292a939b372
lunghyun <nangmannsh@gmail.com>
2026-03-02 21:10:49 +0900

    `test/testcode`: store: RegisterUser 에러 케이스 테스트(Exec, LastInsertID)

--- store/user_test.go
+++ store/user_test.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"errors"
 	"regexp"
 	"testing"
 
@@ -51,12 +52,64 @@ func TestRepository_RegisterUser_DuplicateEntry(t *testing.T) {
 	t.Parallel()
 }
 
-// TODO: RegisterUser: err case
 func TestRepository_RegisterUser_Exec(t *testing.T) {
 	t.Parallel()
+	ctx := context.Background()
+	c := clock.FixedClocker{}
+	errUser := &entity.User{
+		Name:     "errMello",
+		Password: "password",
+		Role:     "User",
+		Created:  c.Now(),
+		Modified: c.Now(),
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { _ = db.Close() })
+
+	sql := `INSERT INTO user(name, password, role, created, modified) VALUES (?, ?, ?, ?, ?)`
+	mock.ExpectExec(regexp.QuoteMeta(sql)).
+		WithArgs(errUser.Name, errUser.Password, errUser.Role, errUser.Created, errUser.Modified).
+		WillReturnError(errors.New("exec failed"))
+
+	xdb := sqlx.NewDb(db, "mysql")
+	r := &Repository{Clocker: c}
+
+	if err = r.RegisterUser(ctx, xdb, errUser); err == nil {
+		t.Error("want no error, but got nil")
+	}
 }
 
-// TODO: RegisterUser: err case
 func TestRepository_RegisterUser_LastInsert(t *testing.T) {
 	t.Parallel()
+	ctx := context.Background()
+	c := clock.FixedClocker{}
+	errUser := &entity.User{
+		Name:     "errMello",
+		Password: "password",
+		Role:     "User",
+		Created:  c.Now(),
+		Modified: c.Now(),
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { _ = db.Close() })
+
+	sql := `INSERT INTO user(name, password, role, created, modified) VALUES (?, ?, ?, ?, ?)`
+	mock.ExpectExec(regexp.QuoteMeta(sql)).
+		WithArgs(errUser.Name, errUser.Password, errUser.Role, errUser.Created, errUser.Modified).
+		WillReturnResult(sqlmock.NewErrorResult(errors.New("last insert id failed")))
+
+	xdb := sqlx.NewDb(db, "mysql")
+	r := &Repository{Clocker: c}
+
+	if err = r.RegisterUser(ctx, xdb, errUser); err == nil {
+		t.Error("want no error, but got nil")
+	}
 }
